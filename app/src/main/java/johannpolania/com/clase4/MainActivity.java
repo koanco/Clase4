26ff3d35baaef80146fa1aba2d96bb2dbde50437
koanco <koanco@hotmail.com>
2016-03-16 15:45:39 -0500

    Initial commit

---
+++ app/src/main/java/johannpolania/com/clase4/MainActivity.java
@@ -0,0 +1,48 @@
+package johannpolania.com.clase4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this,"onCreate",Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"onStart",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this,"onResumen",Toast.LENGTH_SHORT).show();
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,"onStop",Toast.LENGTH_SHORT).show();
+    }
+
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this,"onRestart",Toast.LENGTH_SHORT).show();
+    }
+
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this,"onDestroy",Toast.LENGTH_SHORT).show();
+    }
+
+
+
+
+}
